3c50f8e8c2319c19b366e7e1c1248b82e2f215e6
Sam MAROUN <maroun.sam@gmail.com>
2021-01-04 15:10:06 +0100

    Test for Tour class
    Making first list of test for distanceTo method in City class

--- src/test/java/tsp/lee/jacobson/TourTest.java
+++ src/test/java/tsp/lee/jacobson/TourTest.java
@@ -4,11 +4,99 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 public class TourTest {
 
+	// @Test
+	// public void test() {
+	// 	fail("Not yet implemented");
+	// }
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	/**
+	 * GIVEN getDistance
+	 * WHEN noTour
+	 * THEN 0
+	 */
+
+	public void getDistance_noTour_0(){
+
+		Tour tour = new Tour();
+
+		int expected = 0;
+		int actual = tour.getDistance();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN oneCity
+	 * THEN 0
+	 */
+
+	public void getDistance_oneCity_0(){
+
+		Tour tour = new Tour();
+
+		int expected = 0;
+		int actual = tour.getDistance();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN 2Cities
+	 * THEN 2
+	 */
+
+	public void getDistance_2Cities_2(){
+		ArrayList<City> cities = new ArrayList<City>();
+
+		City FirstCity = new City(0,0);
+		City SecondCity = new City(1,0);
+
+		cities.add(FirstCity);
+		cities.add(SecondCity);
+
+		Tour tour = new Tour(cities);
+
+		int expected = 2;
+		int actual = tour.getDistance();
+
+		assertEquals(expected,actual);
+	}
+
+	@Test
+	/**
+	 * GIVEN getDistance
+	 * WHEN 4Cities
+	 * THEN 2
+	 */
+
+	public void getDistance_4Cities_4(){
+		ArrayList<City> cities = new ArrayList<City>();
+
+		City FirstCity = new City(0,0);
+		City SecondCity = new City(1,0);
+		City ThirdCity = new City(0,1);
+		City FourthCity = new City(1,0);
+
+		cities.add(FirstCity);
+		cities.add(SecondCity);
+		cities.add(ThirdCity);
+		cities.add(FourthCity);
+
+		Tour tour = new Tour(cities);
+
+		int expected = 4;
+		int actual = tour.getDistance();
+
+		assertEquals(expected,actual);
 	}
 
 }
